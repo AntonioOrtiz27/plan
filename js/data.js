38e6869aa676b630d8e2c132044afd6448870df3
Antonio Ortiz <163226248+AntonioOrtiz27@users.noreply.github.com>
2026-09-24 20:37:22 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -24,6 +24,7 @@ const ROUTINE = [
     titulo: "Cardio base · 35'",
     categoria: "pitch",
     ejercicios: [
+      { chip: "10'", texto: "Entrada en calor-Movilidad" },
       { chip: "5'", texto: "Caminata" },
       { chip: "35'", texto: "Trote de 7 series de 5 minutos con intervalos de 90 segundos de caminata." },
       { chip: "5'", texto: "Caminata" },
@@ -34,12 +35,17 @@ const ROUTINE = [
   {
     id: "martes",
     nombre: "MARTES",
-    titulo: "Fuerza + core · 40/50'",
+    titulo: "Fuerza",
     categoria: "pitch",
     ejercicios: [
+      { chip: "10'", texto: "Entrada en calor-Movilidad" },
+      { chip: "3×8-12", texto: "Pecho en Banco Plano" },
+      { chip: "3×8–12", texto: "Biceps" },
+      { chip: "3×20", texto: "Pliometria a una pierna con barra en el piso." },
+      { chip: "3×8–12", texto: "Estocada (por pierna), con una dificultad que te exija (2–3 reps en reserva)\n7kg Mancuerna" },
+      { chip: "3", texto: "Saltos largos luego de hacer estocadas." },
+      
       { chip: "3×20", texto: "puente de glúteo" },
-      { chip: "3×8–12", texto: "sentadilla, con una dificultad que te exija (2–3 reps en reserva)" },
-      { chip: "3×8–12", texto: "estocada (por pierna), con una dificultad que te exija (2–3 reps en reserva)" },
       { chip: "3×15", texto: "abdominales" },
       { chip: "3×12", texto: "elevación de psoas (por lado)" },
       { chip: "3×15", texto: "abs laterales (por lado)" },
@@ -47,24 +53,43 @@ const ROUTINE = [
     ],
     nota: "Fuerza de verdad: menos repeticiones y más carga te sirve más para fútbol que series de 20."
   },
+  
   {
     id: "miercoles",
     nombre: "MIÉRCOLES",
-    titulo: "Cardio resistencia · 35'",
-    categoria: "pitch",
-    ejercicios: [
-      { chip: "5'", texto: "Caminata" },
-      { chip: "30'", texto: "trote continuo, ritmo algo más firme" },
-      { chip: "FIN", texto: "Elongar" }
-    ],
-    nota: "35' es el tiempo que se juega por tiempo."
+    titulo: "Descanso",
+    categoria: "rest",
+    descanso: "Dia de descanso entre martes y jueves.",
+    nota: "Se puede hacer sesión de movilidad/core."
   },
+  
   {
     id: "jueves",
     nombre: "JUEVES",
     titulo: "Velocidad + core · 30/35'",
-    categoria: "clay",
+    categoria: "pitch",
     ejercicios: [
+      { chip: "10'", texto: "Entrada en calor-Movilidad" },
+
+      { chip: "3×8-12", texto: "Remo con barra" },
+      { chip: "3×8-12", texto: "Triceps con barra o mancuerna" },
+      { chip: "3×8-12", texto: "Peso muerto con mancuernas" },
+      { chip: "3×8-12", texto: "Curl nordico" },
+      { chip: "PLIO", texto: "Sesion de Pliometria" },
+      { chip: "PLIO", texto: "Saltos con una pierna" },
+
+      { chip: "FIN", texto: "Elongar" }
+    ],
+    nota: "Más adelante, tras varias semanas de adaptación, se puede pasar a esfuerzos del 90–95% y cambios de dirección."
+  },
+  
+  {
+    id: "viernes",
+    nombre: "VIERNES",
+    titulo: "Cardio - Resistencia",
+    categoria: "pitch",    
+    ejercicios: [
+      { chip: "10'", texto: "Entrada en calor-Movilidad" },
       { chip: "8–10'", texto: "trote suave" },
       { chip: "4×40–50m", texto: "progresiones al 60–70%" },
       { chip: "6×50m", texto: "al 80–85%" },
@@ -75,25 +100,40 @@ const ROUTINE = [
       { chip: "5'", texto: "suave para terminar" },
       { chip: "FIN", texto: "Elongar" }
     ],
-    nota: "Más adelante, tras varias semanas de adaptación, se puede pasar a esfuerzos del 90–95% y cambios de dirección."
-  },
-  {
-    id: "viernes",
-    nombre: "VIERNES",
-    titulo: "Descanso",
-    categoria: "rest",    
-    ejercicios: [
-      { chip: "70'", texto: "Caminata suave" },
-    ],
     nota: "La caminata es opcional, ya que hay que descansar para el partido"
   },
-
   
   {
     id: "sabado",
     nombre: "SÁBADO",
-    titulo: "Partido · 15:30",
+    titulo: "Descanso",
+    categoria: "rest",
+    descanso: "Descanso pre-partido",
+    nota: "Se puede hacer sesión de movilidad/core."
+  },
+  
+  {
+    id: "Domingo",
+    nombre: "DOMINGO",
+    titulo: "Dia de Partido",
     categoria: "gold",
-    descanso: "⚽ Partido a las 15:30."
+    partido: "⚽ Dia de partido"
+  },
+  
+  {
+    id: "Ejercicios",
+    nombre: "Ejercicios Random",
+    titulo: "Ejercicios Random",
+    categoria: "rest",
+    ejercicios: [
+      { chip: "8–10'", texto: "trote suave" },
+      { chip: "4×40–50m", texto: "progresiones al 60–70%" },
+      { chip: "6×50m", texto: "al 80–85%" },
+      { chip: "60–90\"", texto: "recuperación caminando entre repeticiones" },
+      { chip: "3×15", texto: "abdominales" },
+      { chip: "3×12", texto: "elevaciones de piernas" },
+      { chip: "3×15", texto: "abdominales laterales (por lado)" },
+      { chip: "5'", texto: "suave para terminar" },
+      ],
   }
 ];
